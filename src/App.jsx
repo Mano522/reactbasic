0c0273f0d98bc5c27a1197d8479a4b578e6d79c5
manoj kumar k <you@example.com>
2026-09-17 16:07:03 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+function App() {
+  const trainername="Manoj";
+  const trainingday=3;
+  return (
+    <main className="App">
+      <section className="App-header">
+      <p className="daylabel">React day</p>
+      <h2 className="title">College course explore </h2>
+      <p>Trainer:{trainername}</p>
+      <p>Training Day:{trainingday}</p>
+      <p>used react,jsx,componenrs</p>
+    </section>
+    </main>
+  );
+} 
+export default App;
